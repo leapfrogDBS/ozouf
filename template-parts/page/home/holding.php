17f22e97f84d8e944e97b6d116b15a0d94c14a85
David McDonagh <david@leapfrogdbs.co.uk>
2022-06-17 11:05:01 +0100

    Holding Page Mobile Styles

--- template-parts/page/home/holding.php
+++ template-parts/page/home/holding.php
@@ -1,29 +1,29 @@
 <div class="section h-screen bg-yellow" id="holding-page">
     <div class="container flex flex-col justify-between h-full py-10">
-        <div class="row">
-            <div class="col flex flex-col items-end">
-                <div class="flex items-center w-[260px]">
-                    <img class="h-[25px]" src="<?php echo get_template_directory_uri();?>/img/asterisk-black.svg">
-                    <a class="ml-[12px] text-2xl font-semibold" href="tel:004401534780080">+44 (0)1534 780080</a>
+        <div class="row mb-8">
+            <div class="col flex flex-col items-center md:items-end">
+                <div class="flex items-center md:w-[260px]">
+                    <img class="h-[18px] md:h-[25px]" src="<?php echo get_template_directory_uri();?>/img/asterisk-black.svg">
+                    <a class="ml-[12px] text-lg md:text-2xl font-semibold" href="tel:004401534780080">+44 (0)1534 780080</a>
                 </div>
-                <div class="flex items-center w-[260px]">
-                    <img class="h-[25px]" src="<?php echo get_template_directory_uri();?>/img/asterisk-black.svg">
-                    <a class="ml-[12px] text-2xl font-semibold" href="mailto:hello@ozouf.net">hello@ozouf.net</a>
+                <div class="flex items-center md:w-[260px]">
+                    <img class="h-[18px] md:h-[25px]" src="<?php echo get_template_directory_uri();?>/img/asterisk-black.svg">
+                    <a class="ml-[12px] text-lg md:text-2xl font-semibold" href="mailto:hello@ozouf.net">hello@ozouf.net</a>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col flex flex-col mb-[25vh]">
-                <h1 class="uppercase text-center text-2xl font-black tracking-widest mb-[10vh]">New site coming soon</h1>
-                <div id="logo-container" class="w-[280px] m-auto">
+                <h1 class="uppercase text-center text-lg md:text-2xl font-black tracking-widest mb-[10vh]">New site coming soon</h1>
+                <div id="logo-container" class=" w-2/3 sm:w-1/2 md:w-[280px] m-auto">
                     <img class="" src="<?php echo get_template_directory_uri();?>/img/holding-logo.png">
                     <div class="eyes">
                         <div class="eye">
-                            <div class="ball">
+                            <div class="ball w-[8px] h-[8px] sm:w-[10px] sm:h-[10px]">
                             </div>
                         </div>
                         <div class="eye">
-                            <div class="ball">
+                            <div class="ball w-[8px] h-[8px] sm:w-[10px] sm:h-[10px]">
                             </div>
                         </div>
                     </div>    
@@ -32,7 +32,7 @@
         </div>
         <div class="row">
             <div class="col">
-                <h2 class="uppercase text-center text-xl font-bold tracking-widest">brand  |  Web  |  Video  | Animation</h2>
+                <h2 class="uppercase text-center text-lg md:text-xl font-bold tracking-widest">brand  |  Web  |  Video  | Animation</h2>
             </div>
         </div>
     </div>
